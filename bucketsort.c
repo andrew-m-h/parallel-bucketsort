76c16213c2a24bfce821f14bb7e09af114be5f3e
andrew-m-h <andrew.m.h96@gmail.com>
2017-05-01 16:40:38 +1000

    format lines

--- bucketsort.c
+++ bucketsort.c
@@ -37,7 +37,8 @@ int main(int argc, char* argv[]){
 
     input_length = from_string(argv[1]);
 
-    big_bucket_length = (rank == 0) ? (input_length/size + input_length % size) : (input_length / size);
+    big_bucket_length = (rank == 0) ? (input_length/size + input_length % size)
+        : (input_length / size);
     big_bucket = (elem_t*)malloc(big_bucket_length * sizeof(elem_t));
     check_malloc(big_bucket, "big_bucket");
 
@@ -54,8 +55,9 @@ int main(int argc, char* argv[]){
     }
 
     /*Phase 1 - Send big buckets to each process*/
-    MPI_Scatter(input_buffer + (input_length%size), input_length/size, MPI_ELEM_T,
-                big_bucket, input_length/size, MPI_ELEM_T, MAIN_RANK, MPI_COMM_WORLD);
+    MPI_Scatter(input_buffer + (input_length%size), input_length/size,
+                MPI_ELEM_T, big_bucket, input_length/size,
+                MPI_ELEM_T, MAIN_RANK, MPI_COMM_WORLD);
     MPI_Bcast(&min_max, 2, MPI_ELEM_T, MAIN_RANK, MPI_COMM_WORLD);
 
     if (rank == MAIN_RANK)
@@ -67,7 +69,8 @@ int main(int argc, char* argv[]){
 #endif
 
     /*Phase 2 - split data into small buckets*/
-    elem_t * small_buckets = (elem_t*)malloc(big_bucket_length * size * sizeof(elem_t));
+    elem_t * small_buckets = (elem_t*)malloc(big_bucket_length * size *
+                                             sizeof(elem_t));
     check_malloc(small_buckets,"small_buckets");
     size_t * small_bucket_lengths = (size_t*)calloc(size, sizeof(size_t));
     check_malloc(small_bucket_lengths, "small_bucket_lengths");
@@ -77,7 +80,8 @@ int main(int argc, char* argv[]){
     free(big_bucket);
 
     /*Phase 3 - send each of small buckets to the processors*/
-    MPI_Request * small_bucket_reqs = (MPI_Request*)malloc(2*(size-1)*sizeof(MPI_Request));
+    MPI_Request * small_bucket_reqs =
+        (MPI_Request*)malloc(2*(size-1)*sizeof(MPI_Request));
     check_malloc(small_bucket_reqs, "small_bucket_reqs");
 
     size_t small_bucket_reqs_ix = 0;
@@ -91,15 +95,18 @@ int main(int argc, char* argv[]){
 
     for (i=0; (int)i < size; i++){
         if ((int)i not_eq rank){ /*don't send to self*/
-            MPI_Isend(small_bucket_lengths+i, 1, MPI_SIZE_T, i, LENGTH_TAG,
-                      MPI_COMM_WORLD, small_bucket_reqs + small_bucket_reqs_ix++);
-            MPI_Irecv(incoming_lengths + i, 1, MPI_SIZE_T, i, LENGTH_TAG,
-                      MPI_COMM_WORLD, small_bucket_reqs + small_bucket_reqs_ix++);
+            MPI_Isend(small_bucket_lengths+i, 1, MPI_SIZE_T, i,
+                      LENGTH_TAG, MPI_COMM_WORLD, small_bucket_reqs +
+                      small_bucket_reqs_ix++);
+            MPI_Irecv(incoming_lengths + i, 1, MPI_SIZE_T, i,
+                      LENGTH_TAG, MPI_COMM_WORLD, small_bucket_reqs +
+                      small_bucket_reqs_ix++);
         }
     }
 
     for (i=0; (int)i < 2 * (size-1); i++){
-        MPI_Waitany(2 * (size-1), small_bucket_reqs, (int*)&j, MPI_STATUS_IGNORE);
+        MPI_Waitany(2 * (size-1), small_bucket_reqs, (int*)&j,
+                    MPI_STATUS_IGNORE);
     }
 
     for (i=0; (int)i < size; i++)
@@ -116,9 +123,11 @@ int main(int argc, char* argv[]){
         if ((int)i not_eq rank){
             MPI_Isend(small_buckets + i * big_bucket_length,
                       small_bucket_lengths[i], MPI_ELEM_T, i, DATA_TAG,
-                      MPI_COMM_WORLD, small_bucket_reqs + small_bucket_reqs_ix++);
-            MPI_Irecv(to_sort + recv_ix, incoming_lengths[i], MPI_ELEM_T, i, DATA_TAG,
-                      MPI_COMM_WORLD, small_bucket_reqs + small_bucket_reqs_ix++);
+                      MPI_COMM_WORLD, small_bucket_reqs +
+                      small_bucket_reqs_ix++);
+            MPI_Irecv(to_sort + recv_ix, incoming_lengths[i],
+                      MPI_ELEM_T, i, DATA_TAG, MPI_COMM_WORLD,
+                      small_bucket_reqs + small_bucket_reqs_ix++);
         } else {
             memcpy(to_sort + recv_ix, small_buckets + i * big_bucket_length,
                    small_bucket_lengths[i]*sizeof(elem_t));
@@ -129,7 +138,8 @@ int main(int argc, char* argv[]){
     free(incoming_lengths);
 
     for (i=0; (int)i < 2 * (size-1); i++){
-        MPI_Waitany(2 * (size-1), small_bucket_reqs, (int*)&j, MPI_STATUS_IGNORE);
+        MPI_Waitany(2 * (size-1), small_bucket_reqs, (int*)&j,
+                    MPI_STATUS_IGNORE);
     }
 
     free(small_buckets);
@@ -171,17 +181,22 @@ static inline void range(size_t len, elem_t* buf, elem_t extremes[2]){
     extremes[1] = max;
 }
 
-static inline void split_small_buckets(size_t* small_bucket_lengths, elem_t* small_buckets){
+static inline void split_small_buckets(size_t* small_bucket_lengths,
+                                       elem_t* small_buckets){
     size_t i, j;
-    const ssize_t small_bucket_range = ((ssize_t)max_value - (ssize_t)min_value)/(ssize_t)size;
+    const ssize_t small_bucket_range =
+        ((ssize_t)max_value - (ssize_t)min_value)/(ssize_t)size;
 
     for (i=0; i < big_bucket_length; i++){
         for (j=0; (int)j < size; j++){
-            if ((ssize_t)big_bucket[i] <= ((ssize_t)min_value + ((ssize_t)j+1) * small_bucket_range)){
-                small_buckets[j*big_bucket_length + small_bucket_lengths[j]++] = big_bucket[i];
+            if ((ssize_t)big_bucket[i] <=
+                ((ssize_t)min_value + ((ssize_t)j+1) * small_bucket_range)){
+                small_buckets[j*big_bucket_length + small_bucket_lengths[j]++] =
+                    big_bucket[i];
                 break;
             } else if ((int)j == size-1){
-                small_buckets[j*big_bucket_length + small_bucket_lengths[j]++] = big_bucket[i];
+                small_buckets[j*big_bucket_length + small_bucket_lengths[j]++] =
+                    big_bucket[i];
                 break;
             }
         }
@@ -199,15 +214,19 @@ static inline void output_sorted(size_t to_sort_length, elem_t* to_sort){
 
         for (i=0; i < to_sort_length; i++) print_elem_ln(stdout, to_sort[i]);
         fflush(stdout);
+
         if (size > 1)
             MPI_Send(&i, 1, MPI_INT, rank+1, SIGNAL_TAG, MPI_COMM_WORLD);
     } else if (rank == size -1){
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         for (i=0; i<to_sort_length; i++) print_elem_ln(stdout, to_sort[i]);
         fflush(stdout);
+
     } else {
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         for (i=0; i<to_sort_length; i++) print_elem_ln(stdout, to_sort[i]);
         fflush(stdout);
@@ -224,6 +243,7 @@ static void debug_1(){
         fprintf(stderr, "Process %d\n", rank);
         fprintf(stderr, "Min Value: "); print_elem_ln(stderr, min_value);
         fprintf(stderr, "Max Value: "); print_elem_ln(stderr, max_value);
+
         for (i=0;i<big_bucket_length;i++) print_elem_ln(stderr, big_bucket[i]);
 
         fflush(stderr);
@@ -232,7 +252,8 @@ static void debug_1(){
             MPI_Send(&i, 1, MPI_INT, rank+1, SIGNAL_TAG, MPI_COMM_WORLD);
 
     }else if (rank == size-1){
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         fprintf(stderr, "Process %d\n", rank);
         fprintf(stderr, "Min Value: "); print_elem_ln(stderr, min_value);
@@ -242,7 +263,8 @@ static void debug_1(){
 
         fflush(stderr);
     } else {
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         fprintf(stderr, "Process %d\n", rank);
         fprintf(stderr, "Min Value: "); print_elem_ln(stderr, min_value);
@@ -268,13 +290,15 @@ static void debug_2(size_t to_sort_length, elem_t* to_sort){
         if (size > 1)
             MPI_Send(&i, 1, MPI_INT, rank+1, SIGNAL_TAG, MPI_COMM_WORLD);
     }else if (rank == size-1){
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         fprintf(stderr, "Process %d\n", rank);
         for (i=0;i<to_sort_length;i++) print_elem_ln(stderr, to_sort[i]);
         fflush(stderr);
     } else {
-        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&i, 1, MPI_INT, rank-1, SIGNAL_TAG,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         fprintf(stderr, "Process %d\n", rank);
         for (i=0; i<to_sort_length; i++) print_elem_ln(stderr, to_sort[i]);
